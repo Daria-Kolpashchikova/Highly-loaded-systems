f6163e8d6b861f0d90b202272d037843a4a03ffa
Daria Kolpashchikova <58345056+Daria-Kolpashchikova@users.noreply.github.com>
2021-10-04 16:38:48 +0300

    Update task1(1).cpp

--- MPI/task1(1).cpp
+++ MPI/task1(1).cpp
@@ -11,13 +11,13 @@ int main(int argc, char** argv)
 {
     int size, rank;
 
-	MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
 	
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
-	printf("Total number of processes in the communicator: %d, current procces number: %d\n", size, rank);
+    printf("Total number of processes in the communicator: %d, current procces number: %d\n", size, rank);
 	
     MPI_Finalize();
-	return 0;
+    return 0;
 }
